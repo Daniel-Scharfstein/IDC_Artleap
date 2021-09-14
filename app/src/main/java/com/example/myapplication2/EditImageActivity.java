d5741bbfbffd42408c42978d35a1710df11b2fb5
danielscharfstein <daniel.scharfstein@behalf.com>
2021-09-13 20:13:24 -0400

    adding checkbox vi and checkbox x

--- app/src/main/java/com/example/myapplication2/EditImageActivity.java
+++ app/src/main/java/com/example/myapplication2/EditImageActivity.java
@@ -15,13 +15,12 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.SeekBar;
-import android.widget.Toast;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintLayout;
 
 import com.example.myapplication2.openglHelper.PictureRenderer;
+import com.example.myapplication2.utils.EditParameters;
 
 import java.io.File;
 import java.util.Objects;
@@ -30,6 +29,7 @@ public class EditImageActivity extends AppCompatActivity {
     public static final int PINK = 0xFFF15ECF;
     MenuItem menuItem;
     private GLSurfaceView gLView;
+    public EditParameters editParameters = new EditParameters(0.0, 0.0);
 
 
     @Override
@@ -45,21 +45,21 @@ public class EditImageActivity extends AppCompatActivity {
         ImageButton colorButton = findViewById(R.id.colorButton);
         ImageButton angleButton = findViewById(R.id.angleButton);
         ImageButton spreadButton = findViewById(R.id.spreadButton);
+        ImageButton xBox = findViewById(R.id.xBox);
+        ImageButton checkBox = findViewById(R.id.checkBox);
+
+        xBox.setVisibility(View.GONE);
+        checkBox.setVisibility(View.GONE);
 
         skbar.setVisibility(View.GONE);
-        showSeekBar(skbar, colorButton, angleButton, spreadButton);
+        showSeekBar(skbar, colorButton, angleButton, spreadButton, xBox, checkBox);
 
-        ConstraintLayout rlayout = (ConstraintLayout) findViewById(R.id.allPage);
-        rlayout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                skbar.setVisibility(View.GONE);
-                skbar.setProgress(0);
-                colorButton.setVisibility(View.VISIBLE);
-                angleButton.setVisibility(View.VISIBLE);
-                spreadButton.setVisibility(View.VISIBLE);
-            }
+        xBox.setOnClickListener(ib -> {
+            backToEditClick(skbar, colorButton, angleButton, spreadButton, xBox, checkBox);
+        });
 
+        checkBox.setOnClickListener(ib -> {
+            backToEditClick(skbar, colorButton, angleButton, spreadButton, xBox, checkBox);
         });
 
         openImageWithOpenGl();
@@ -67,25 +67,71 @@ public class EditImageActivity extends AppCompatActivity {
 
     }
 
-    public void showSeekBar(SeekBar skbar, ImageButton colorButton, ImageButton angleButton, ImageButton spreadButton) {
+    public void backToEditClick(SeekBar skbar, ImageButton colorButton, ImageButton angleButton, ImageButton spreadButton, ImageButton xBox, ImageButton checkBox) {
+        skbar.setVisibility(View.GONE);
+        xBox.setVisibility(View.GONE);
+        checkBox.setVisibility(View.GONE);
+        colorButton.setVisibility(View.VISIBLE);
+        angleButton.setVisibility(View.VISIBLE);
+        spreadButton.setVisibility(View.VISIBLE);
+        System.out.println(editParameters.getColor() + "  " + editParameters.getAngle());
+    }
+
+    public void showSeekBar(SeekBar skbar, ImageButton colorButton, ImageButton angleButton, ImageButton spreadButton, ImageButton xBox, ImageButton checkBox) {
         colorButton.setOnClickListener(ib -> {
-            //For changing visibility
             angleButton.setVisibility(View.GONE);
             spreadButton.setVisibility(View.GONE);
             skbar.setVisibility(View.VISIBLE);
+            xBox.setVisibility(View.VISIBLE);
+            checkBox.setVisibility(View.VISIBLE);
+
+            skbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+                @Override
+                public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                    System.out.println("in color");
+                    editParameters.setColor(seekBar.getProgress());
+                }
+
+                @Override
+                public void onStartTrackingTouch(SeekBar seekBar) {
+
+                }
+
+                @Override
+                public void onStopTrackingTouch(SeekBar seekBar) {
+
+                }
+            });
+            skbar.setProgress(0);
         });
         angleButton.setOnClickListener(ib -> {
             //For changing visibility
             colorButton.setVisibility(View.GONE);
             spreadButton.setVisibility(View.GONE);
             skbar.setVisibility(View.VISIBLE);
+            xBox.setVisibility(View.VISIBLE);
+            checkBox.setVisibility(View.VISIBLE);
+            skbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+                @Override
+                public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                    System.out.println("in angle");
+                    editParameters.setAngle(seekBar.getProgress());
+                }
+
+                @Override
+                public void onStartTrackingTouch(SeekBar seekBar) {
+
+                }
+
+                @Override
+                public void onStopTrackingTouch(SeekBar seekBar) {
+
+                }
+
+            });
+            skbar.setProgress(0);
         });
-        spreadButton.setOnClickListener(ib -> {
-            //For changing visibility
-            colorButton.setVisibility(View.GONE);
-            angleButton.setVisibility(View.GONE);
-            skbar.setVisibility(View.VISIBLE);
-        });
+
     }
 
     public void setActionBar() {
@@ -125,7 +171,6 @@ public class EditImageActivity extends AppCompatActivity {
         Bitmap picture = BitmapFactory.decodeFile(file.getAbsolutePath());
 
         if (supportsEs2) {
-            setContentView(R.layout.edit_photo_page);
             gLView = (GLSurfaceView) this.findViewById(R.id.check);
             gLView.setEGLContextClientVersion(2);
             PictureRenderer renderer = new PictureRenderer(getApplicationContext());
---
+++ app/src/main/java/com/example/myapplication2/utils/EditParameters.java
@@ -0,0 +1,27 @@
+package com.example.myapplication2.utils;
+
+public class EditParameters {
+    double color;
+    double angle;
+
+    public EditParameters(double color, double angle) {
+        setColor(color);
+        setAngle(angle);
+    }
+
+    public double getColor() {
+        return color;
+    }
+
+    public void setColor(double color) {
+        this.color = color;
+    }
+
+    public double getAngle() {
+        return angle;
+    }
+
+    public void setAngle(double angle) {
+        this.angle = angle;
+    }
+}
