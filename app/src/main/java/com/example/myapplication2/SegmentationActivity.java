c2b51ff2cdee667e48662a708893053bc6c008c7
idobichler <idobichler@gmail.com>
2021-09-22 15:34:03 +0300

    for scharf with color change 2

--- app/src/main/java/com/example/myapplication2/SegmentationActivity.java
+++ app/src/main/java/com/example/myapplication2/SegmentationActivity.java
@@ -7,6 +7,7 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
 
 import androidx.annotation.ColorInt;
 import androidx.appcompat.app.AppCompatActivity;
